4443162fd8976263f48a7b1e2817c5b6e5295f34
kingvaboor <151428291+kingvaboor@users.noreply.github.com>
2023-11-29 00:20:54 +0530

    Update tbt.c

--- tbt.c
+++ tbt.c
@@ -1,42 +1,72 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
+
+// Structure for the threaded binary tree node
 struct node
 {
     int data;
     int lbit, rbit;
     struct node *left, *right;
 };
+
+// Function to create a new node and insert it into the threaded binary tree
 struct node *TBTcreate(struct node *head, int data);
+
+// Function to perform preorder traversal on the threaded binary tree
 void preorderTBT(struct node *head);
+
+// Function to perform inorder traversal on the threaded binary tree
 void inorderTBT(struct node *head);
-void postorderTBT(struct node *head);
+
+// Function to find the inorder successor of a node in the threaded binary tree
 struct node *inorder_succ(struct node *T);
+
+// Function to perform postorder traversal on the threaded binary tree
+void postorderTBT(struct node *head);
+
 int main()
 {
     struct node *head;
+
+    // Allocate memory for the head of the threaded binary tree
     head = (struct node *)malloc(sizeof(struct node));
-    int key, n;
+
+    // Initialize the head with dummy values
     head->lbit = head->rbit = 1;
     head->left = head->right = head;
+
+    // Create a threaded binary tree by inserting nodes
     head = TBTcreate(head, 10);
     head = TBTcreate(head, 6);
     head = TBTcreate(head, 15);
     head = TBTcreate(head, 20);
     head = TBTcreate(head, 17);
-    printf("Prorder traversal : ");
+
+    // Display the threaded binary tree using different traversal methods
+    printf("Preorder traversal: ");
     preorderTBT(head);
-    printf("\ninrder traversal : ");
+
+    printf("\nInorder traversal: ");
     inorderTBT(head);
-    printf("\npostrder traversal : ");
+
+    printf("\nPostorder traversal: ");
     postorderTBT(head->left);
+
+    return 0;
 }
+
+// Function to create a new node and insert it into the threaded binary tree
 struct node *TBTcreate(struct node *head, int data)
 {
     struct node *temp, *p, *q;
+
+    // Allocate memory for the new node
     temp = (struct node *)malloc(sizeof(struct node));
     temp->data = data;
     temp->lbit = temp->rbit = 0;
+
+    // Check if the tree is empty
     if (head->left == head)
     {
         temp->left = head;
@@ -44,6 +74,8 @@ struct node *TBTcreate(struct node *head, int data)
         head->left = temp;
         return head;
     }
+
+    // Traverse the tree to find the correct position for the new node
     p = head->left;
     while (1)
     {
@@ -60,6 +92,8 @@ struct node *TBTcreate(struct node *head, int data)
             break;
         }
     }
+
+    // Update the pointers to insert the new node
     q = p;
     if (data > q->data)
     {
@@ -75,23 +109,33 @@ struct node *TBTcreate(struct node *head, int data)
         q->left = temp;
         q->lbit = 1;
     }
+
     return head;
 }
+
+// Function to perform preorder traversal on the threaded binary tree
 void preorderTBT(struct node *head)
 {
     struct node *T;
+
+    // Start from the leftmost node
     T = head->left;
+
     while (T != head)
     {
         printf("%d ", T->data);
+
+        // Move to the left child if it exists
         if (T->lbit == 1)
         {
             T = T->left;
         }
+        // Move to the right child if it exists
         else if (T->rbit == 1)
         {
             T = T->right;
         }
+        // If there is no left or right child, move to the threaded node
         else
         {
             while (T->rbit == 0)
@@ -102,27 +146,40 @@ void preorderTBT(struct node *head)
         }
     }
 }
+
+// Function to perform inorder traversal on the threaded binary tree
 void inorderTBT(struct node *head)
 {
     struct node *T;
+
+    // Start from the leftmost node
     T = head->left;
+
+    // Move to the leftmost leaf node
     while (T->lbit == 1)
     {
         T = T->left;
     }
+
+    // Traverse the threaded binary tree in inorder
     while (T != head)
     {
         printf("%d ", T->data);
+
+        // Move to the inorder successor
         T = inorder_succ(T);
     }
-    return;
 }
+
+// Function to find the inorder successor of a node in the threaded binary tree
 struct node *inorder_succ(struct node *T)
 {
     if (T->rbit == 0)
     {
         return (T->right);
     }
+
+    // If there is a right child, find the leftmost node in the right subtree
     if (T->rbit == 1)
     {
         T = T->right;
@@ -133,8 +190,11 @@ struct node *inorder_succ(struct node *T)
         return T;
     }
 }
+
+// Function to perform postorder traversal on the threaded binary tree
 void postorderTBT(struct node *head)
 {
+    // Base case: if the tree is empty, return
     if (head->lbit == 1)
     {
         postorderTBT(head->left);
@@ -144,5 +204,4 @@ void postorderTBT(struct node *head)
         postorderTBT(head->right);
     }
     printf("%d ", head->data);
-    return;
 }
